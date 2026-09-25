c8fc2d32fad2d03cd5ba27e0a7ec4b5826ec1b8e
Leo-0107 <linm25280@gmail.com>
2026-09-25 10:12:24 +0900

    Fix offline startup and sleep timer

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE_NAME = "music-player-v1";
+const CACHE_NAME = "music-player-v2";
 const APP_SHELL = [
   "./",
-  "./music.html",
+  "./index.html",
   "./manifest.json",
   "./icon-192.png",
   "./icon-512.png"
@@ -50,7 +50,7 @@ self.addEventListener("fetch", (event) => {
           caches.open(CACHE_NAME).then((cache) => cache.put(request, copy)).catch(() => {});
           return response;
         })
-        .catch(() => cached || caches.match("./music.html"));
+        .catch(() => cached || caches.match("./index.html"));
     })
   );
 });
