ecbafee9e3400d9064ee20cd53a838d070278013
Leo-0107 <linm25280@gmail.com>
2026-09-25 16:49:04 +0900

    Address player storage and PWA reliability issues

--- sw.js
+++ sw.js
@@ -50,7 +50,10 @@ self.addEventListener("fetch", (event) => {
           caches.open(CACHE_NAME).then((cache) => cache.put(request, copy)).catch(() => {});
           return response;
         })
-        .catch(() => cached || caches.match("./index.html"));
+        .catch(() => {
+          if (request.mode === "navigate") return caches.match("./index.html");
+          return Response.error();
+        });
     })
   );
 });
